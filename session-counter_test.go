ee8bfe859463292b15928ad0434e3712f54956ac
Matt Jadud <matt@jadud.com>
2021-03-24 10:01:58 -0400

    Tests passing again
    This uses 'ticks' instead of real time. It's hard to test the algorithm
    if we use actual clock time. I'm not happy about it, per se, but it will
    "work."

--- session-counter_test.go
+++ session-counter_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"sync"
 	"testing"
 
@@ -38,23 +37,23 @@ func hashes(arr ...string) [][]string {
 // in this test set that "next" will always be UID 0.
 // (If "next" and "apple" are together)
 var m = map[string]string{
-	"next":     "00:00:0f", // ID 0
-	"ericsson": "00:01:ec",
-	"apple":    "00:03:93",
+	"next":     "00:00:0f:aa:bb:cc", // ID 0
+	"ericsson": "00:01:ec:aa:bb:cc",
+	"apple":    "00:03:93:aa:bb:cc",
+	"next2":    "00:00:0f:ee:ff:00",
 }
 
 var tests = []struct {
-	description          string
-	passfail             bool
-	uniqueness_window    int
-	disconnection_window int
-	initMap              []string
-	loopMaps             [][]string
-	resultMap            map[string]int
+	description       string
+	passfail          bool
+	uniqueness_window int
+	initMap           []string
+	loopMaps          [][]string
+	resultMap         map[string]int
 }{
 	// One input hash.
 	{"one input mac, one loop mac",
-		PASS, 10, 5,
+		PASS, 5,
 		macs(m["next"]),
 		hashes(m["next"]),
 		map[string]int{
@@ -63,7 +62,7 @@ var tests = []struct {
 	},
 	// // Two input hashes
 	{"two input macs, one loop mac",
-		PASS, 10, 5,
+		PASS, 5,
 		macs(m["next"], m["apple"]),
 		hashes(m["next"]),
 		// Why zero and one?
@@ -75,63 +74,77 @@ var tests = []struct {
 			"Apple:1": 1,
 		},
 	},
-	// // Three hashes, three minutes
-	// {"three input macs, three comms in the middle",
-	// 	PASS, 10, 5,
-	// 	// Next, Apple, Ericsson
-	// 	macs(m["next"], m["apple"], m["ericsson"]),
-	// 	hashes("de:ad:be:ef", "de:ad:be:ef", "de:ad:be:ef"),
-	// 	// IDs will be assigned by MAC address sort!
-	// 	map[*model.UserMapping]int{
-	// 		{Mfg: "Next", Id: 0}:     3,
-	// 		{Mfg: "Apple", Id: 2}:    3,
-	// 		{Mfg: "Ericsson", Id: 1}: 3,
-	// 		{Mfg: "unknown", Id: 3}:  0,
-	// 	},
-	// },
+	// // Two input hashes
+	{"two input macs, one loop mac, both next",
+		PASS, 5,
+		macs(m["next"], m["next2"]),
+		hashes(m["next"]),
+		// Why zero and one?
+		// Zero for deadbeef, because we send it in the loop.
+		// One for beefcafe, because it was only sent once, and
+		// one tick goes by.
+		map[string]int{
+			"Next:0": 0,
+			"Next:1": 1,
+		},
+	},
+	// Three hashes, three minutes
+	{"three input macs, three comms in the middle",
+		PASS, 5,
+		// Next, Apple, Ericsson
+		macs(m["next"], m["apple"], m["ericsson"]),
+		hashes("de:ad:be:ef", "de:ad:be:ef", "de:ad:be:ef"),
+		// IDs will be assigned by MAC address sort!
+		map[string]int{
+			"Next:0":     3,
+			"Apple:1":    3,
+			"Ericsson:2": 3,
+			"unknown:3":  0,
+		},
+	},
 
-	// // Next times out, because it is considered to
-	// // have "disconnected" after 5 minutes.
-	// {"Next should disappear",
-	// 	PASS, 10, 5,
-	// 	macs(m["next"], m["apple"], m["ericsson"]),
-	// 	hashes(
-	// 		"de:ad:be:ef",
-	// 		"de:ad:be:ef",
-	// 		"de:ad:be:ef",
-	// 		m["apple"],
-	// 		m["ericsson"]),
-	// 	// Why zero and one?
-	// 	// Zero for deadbeef, because we send it in the loop.
-	// 	// One for beefcafe, because it was only sent once, and
-	// 	// one tick goes by.
-	// 	map[*model.UserMapping]int{
-	// 		{Mfg: "Apple", Id: 2}:    1,
-	// 		{Mfg: "Ericsson", Id: 1}: 0,
-	// 		{Mfg: "unknown", Id: 3}:  2,
-	// 	},
-	// },
+	// Next times out, because it is considered to
+	// have "disconnected" after 5 minutes.
+	{"Next should disappear",
+		PASS, 5,
+		macs(m["next"], m["apple"], m["ericsson"]),
+		hashes(
+			"de:ad:be:ef",
+			"de:ad:be:ef",
+			"de:ad:be:ef",
+			m["apple"],
+			m["ericsson"]),
+		// Why zero and one?
+		// Zero for deadbeef, because we send it in the loop.
+		// One for beefcafe, because it was only sent once, and
+		// one tick goes by.
+		map[string]int{
+			"Apple:1":    1,
+			"Ericsson:2": 0,
+			"unknown:3":  2,
+		},
+	},
 
-	// // Next times out, comes back. Still ID 0.
-	// // Apple is considered to have disconnected.
-	// {"Drop two",
-	// 	PASS, 10, 5,
-	// 	macs(m["next"], m["apple"], m["ericsson"]),
-	// 	hashes(
-	// 		"de:ad:be:ef",
-	// 		"de:ad:be:ef",
-	// 		"de:ad:be:ef",
-	// 		"de:ad:be:ef",
-	// 		m["next"]),
-	// 	// Why zero and one?
-	// 	// Zero for deadbeef, because we send it in the loop.
-	// 	// One for beefcafe, because it was only sent once, and
-	// 	// one tick goes by.
-	// 	map[*model.UserMapping]int{
-	// 		{Mfg: "Next", Id: 0}:    0,
-	// 		{Mfg: "unknown", Id: 3}: 1,
-	// 	},
-	// },
+	// Next times out, comes back. Still ID 0.
+	// Apple is considered to have disconnected.
+	{"Drop two",
+		PASS, 5,
+		macs(m["next"], m["apple"], m["ericsson"]),
+		hashes(
+			"de:ad:be:ef",
+			"de:ad:be:ef",
+			"de:ad:be:ef",
+			"de:ad:be:ef",
+			m["next"]),
+		// Why zero and one?
+		// Zero for deadbeef, because we send it in the loop.
+		// One for beefcafe, because it was only sent once, and
+		// one tick goes by.
+		map[string]int{
+			"Next:0":    0,
+			"unknown:3": 1,
+		},
+	},
 }
 
 func assertEqual(t *testing.T, a interface{}, b interface{}, message string) {
@@ -169,7 +182,6 @@ func TestRawToUid(t *testing.T) {
 	for testNdx, e := range tests {
 		t.Logf("Test #%v: %v\n", testNdx, e.description)
 		cfg.Monitoring.UniquenessWindow = e.uniqueness_window
-		cfg.Monitoring.DisconnectionWindow = e.disconnection_window
 		var wg sync.WaitGroup
 
 		ch_macs := make(chan []string)
@@ -194,10 +206,9 @@ func TestRawToUid(t *testing.T) {
 			// The init map
 			<-ch_uniq
 			count := len(e.loopMaps) - 1
-			//t.Log("receiving ", count)
 			for i := 0; i < count; i++ {
+				// This reads in the intervening maps.
 				<-ch_uniq
-				//t.Log("receive ", h)
 			}
 			u = <-ch_uniq
 			ch_poison <- true
@@ -209,7 +220,7 @@ func TestRawToUid(t *testing.T) {
 		// The last value we receive needs to have its time updated.
 		expected := fmt.Sprint(e.resultMap)
 		received := fmt.Sprint(u)
-		log.Println("expected", expected, "received", received)
+		//log.Println("expected", expected, "received", received)
 
 		if e.passfail {
 			assertEqual(t, expected, received, "not equal")
--- tlp/algorithm_two.go
+++ tlp/algorithm_two.go
@@ -3,18 +3,19 @@ package tlp
 import (
 	"fmt"
 	"log"
-	"time"
 
 	"gsa.gov/18f/session-counter/api"
 	"gsa.gov/18f/session-counter/config"
 	"gsa.gov/18f/session-counter/csp"
 )
 
+// This probably should be a proper database.
 type uniqueMappingDB struct {
-	lastid    *int
-	uid       map[string]int
-	mfg       map[string]string
-	timestamp map[string]string
+	lastid *int
+	uid    map[string]int
+	mfg    map[string]string
+	// timestamp map[string]string
+	tick map[string]int
 }
 
 func newUMDB() *uniqueMappingDB {
@@ -24,46 +25,55 @@ func newUMDB() *uniqueMappingDB {
 	// umdb.mfg = make(map[string]string)
 	// umdb.timestamp = make(map[string]time.Time)
 	umdb := &uniqueMappingDB{
-		lastid:    new(int),
-		uid:       make(map[string]int),
-		mfg:       make(map[string]string),
-		timestamp: make(map[string]string)}
+		lastid: new(int),
+		uid:    make(map[string]int),
+		mfg:    make(map[string]string),
+		// timestamp: make(map[string]string),
+		tick: make(map[string]int)}
 	return umdb
 }
 
-func (umdb uniqueMappingDB) updateMapping(cfg *config.Config, mac string) {
-	_, found := umdb.mfg[mac]
+func (umdb uniqueMappingDB) advanceTime() {
+	// Bump all the ticks by one.
+	for mac, _ := range umdb.mfg {
+		umdb.tick[mac] = umdb.tick[mac] + 1
+	}
+}
 
-	// If we didn't find it, then we need to add it.
+func (umdb uniqueMappingDB) updateMapping(cfg *config.Config, mac string) {
+
+	_, found := umdb.mfg[mac]
+	// If we didn't find the mac we're supposed to update, then we need to add it.
 	if !found {
 		// Assign the next id.
 		umdb.uid[mac] = *umdb.lastid
-		log.Println("mapping", mac, "to uid", *umdb.lastid)
 		// Increment for the next found address.
 		*umdb.lastid = *umdb.lastid + 1
-		log.Println("bumped uid to", *umdb.lastid)
 		// Grab a manufacturer for this MAC
 		umdb.mfg[mac] = api.Mac_to_mfg(cfg, mac)
 		// Say when we saw it.
-		now := time.Now().Format(time.RFC3339)
-		umdb.timestamp[mac] = now
+		// now := time.Now().Format(time.RFC3339)
+		// umdb.timestamp[mac] = now
+		umdb.tick[mac] = 0
 	} else {
 		// If this address is already known, update
 		// when we last saw it.
-		umdb.timestamp[mac] = time.Now().Format(time.RFC3339)
+		// umdb.timestamp[mac] = time.Now().Format(time.RFC3339)
+		umdb.tick[mac] = 0
 	}
 }
 
 func (umdb uniqueMappingDB) removeOldMappings(window int) {
-	now := time.Now()
+	// now := time.Now()
 	remove := make([]string, 0)
 	// Find everything we need to remove.
 	for mac, _ := range umdb.mfg {
-		storedtime, _ := time.Parse(time.RFC3339, umdb.timestamp[mac])
-		diff := now.Sub(storedtime)
+		// storedtime, _ := time.Parse(time.RFC3339, umdb.timestamp[mac])
+		// diff := now.Sub(storedtime)
 		// Is it further in the past than our window (in minutes)?
-		if int(diff.Minutes()) > window {
-			log.Println(mac, "is old. removing. diff:", diff.Minutes())
+		//if (int(diff.Minutes()) > window) || (umdb.tick[mac] >= window) {
+		if umdb.tick[mac] >= window {
+			log.Println(mac, "is old. removing. tick:", umdb.tick[mac])
 			remove = append(remove, mac)
 		}
 	}
@@ -71,19 +81,21 @@ func (umdb uniqueMappingDB) removeOldMappings(window int) {
 	for _, mac := range remove {
 		delete(umdb.uid, mac)
 		delete(umdb.mfg, mac)
-		delete(umdb.timestamp, mac)
+		// delete(umdb.timestamp, mac)
+		delete(umdb.tick, mac)
 	}
 }
 
 func (umdb uniqueMappingDB) asUserMappings() map[string]int {
 	h := make(map[string]int)
-	n := time.Now()
+	// n := time.Now()
 
 	for mac, _ := range umdb.mfg {
 		userm := fmt.Sprintf("%v:%d", umdb.mfg[mac], umdb.uid[mac])
-		storedtime, _ := time.Parse(time.RFC3339, umdb.timestamp[mac])
-		diff := n.Sub(storedtime)
-		h[userm] = int(diff.Minutes())
+		// storedtime, _ := time.Parse(time.RFC3339, umdb.timestamp[mac])
+		// diff := n.Sub(storedtime)
+		// h[userm] = int(diff.Minutes())
+		h[userm] = umdb.tick[mac]
 	}
 
 	return h
@@ -124,6 +136,9 @@ func AlgorithmTwo(ka *csp.Keepalive, cfg *config.Config, in <-chan []string, out
 		case <-ping:
 			pong <- "AlgorithmTwo"
 		case arr := <-in:
+			// If we consider every message a "tick" of the clock, we need to advance time.
+			umdb.advanceTime()
+
 			// We get in a list of MAC addresses. Create mappings.
 			// Timestamp everything as we see it, new or old.
 			for _, mac := range arr {
