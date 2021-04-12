edfda3cf651d095ce3dbcac239123ee6acb81ec9
Matt Jadud <matthew.jadud@gsa.gov>
2021-04-12 14:27:38 -0400

    Ready for testing/integration.

--- read-initial-configuration.go
+++ read-initial-configuration.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
+	"log"
 	"math"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 
 	"github.com/acarl005/stripansi"
 	"github.com/fatih/color"
-	"github.com/jedib0t/go-pretty/v6/text"
 	"gsa.gov/18f/read-initial-configuration/wordlist"
 )
 
@@ -24,8 +23,9 @@ const yamlPath = "/etc/session-counter/auth.yaml"
 const states = "(AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY|AS|DC|FM|GU|MH|MP|PW|PR|VI)"
 
 type config struct {
-	Token string `yaml:"token"`
-	FCFS  string `yaml:"fcfs"`
+	Token string `yaml:"api_token"`
+	FCFS  string `yaml:"fcfs_seq_id"`
+	Tag   string `yaml:"tag"`
 }
 
 func decode(ndx int) string {
@@ -43,10 +43,12 @@ func decode(ndx int) string {
 }
 
 func readFCFS() string {
-	fmt.Print(text.FgYellow.Sprint("Look up and enter the FCFS Id Seq for this device's location: "))
-	fmt.Println("https://www.imls.gov/search-compare/")
-	fmt.Print(text.FgYellow.Sprint("It should look like: "))
-	fmt.Println(text.FgWhite.Sprint("KY0069-003\n"))
+	msg := ""
+	msg += "Look up and enter the FCFS Id Seq for this device's location\n\n"
+	msg += color.BlueString("https://www.imls.gov/search-compare/\n\n")
+	msg += "The FCFS Seq Id should look like: "
+	msg += color.New(color.FgYellow).Sprint("KY0069-003")
+	fmt.Println(box(color.New(color.FgBlue), msg))
 
 	reader := bufio.NewReader(os.Stdin)
 	re := regexp.MustCompile(states + `[0-9]{4}-[0-9]{3}`)
@@ -61,13 +63,19 @@ func readFCFS() string {
 		if re.MatchString(fcfsid) {
 			matched = true
 		} else {
-			fmt.Println(text.FgRed.Sprint("That does not seem to be a full sequence ID."))
+			color.New(color.FgRed).Println("\nThat does not seem to be a full sequence ID.")
+			color.New(color.FgYellow).Println("It should be a state abbreviation, four digits, a hyphen, and three more digits.")
 			fmt.Printf("Please try again.\n\n")
 		}
 	}
 
-	fmt.Println(text.FgGreen.Sprint("\nGreat! Thank you!"))
-	fmt.Println(text.FgWhite.Sprint("---------\n"))
+	fmt.Println()
+	yay := box(color.New(color.FgHiGreen), color.New(color.FgGreen).Sprint("Great! Thank you!"))
+	fmt.Print(yay)
+
+	// We will pause, because people might get confused if it goes too fast.
+	time.Sleep(2 * time.Second)
+
 	return fcfsid
 }
 
@@ -80,7 +88,7 @@ func box(c *color.Color, s string) string {
 		// String color codes before measuring
 		s = stripansi.Strip(s)
 		if len(s) > max {
-			fmt.Println(s, len(s))
+			// fmt.Println(s, len(s))
 			max = len(s)
 		}
 	}
@@ -141,11 +149,11 @@ func readToken() string {
 		} else {
 			ndx, err := wordlist.GetPairIndex(pair)
 			if err != nil {
-				fmt.Println(text.FgRed.Sprint("\n[ BAD! ] I can't find that word pair. Please try again, or DONE if you have no more word pairs.\n"))
+				color.New(color.FgBlue).Println("\n[ BAD! ] I can't find that word pair. Please try again, or DONE if you have no more word pairs.\n")
 			} else {
 				wpndx += 1
 				decoded := decode(ndx)
-				fmt.Println(text.FgGreen.Sprintf("\n[ GOOD! ] `%v` became `%v`\n", pair, decoded))
+				color.New(color.FgGreen).Printf("\n[ GOOD! ] `%v` became `%v`\n\n", pair, decoded)
 				key += decoded
 			}
 		}
@@ -153,19 +161,55 @@ func readToken() string {
 	return key
 }
 
-func writeYAML(cfg *config) {
-	s, err := json.Marshal(cfg)
+func readTag() string {
+	msg := ""
+	msg += "Enter your device tag.\n\n"
+	msg += "This will end up in the data, and will help you identify the device.\n\n"
+	msg += "Examples:\n\n"
+	msg += "\t1) " + color.New(color.FgYellow).Sprint("behind refdesk") + "\n"
+	msg += "\t2) " + color.New(color.FgYellow).Sprint("in collections") + "\n"
+	msg += "\t3) " + color.New(color.FgYellow).Sprint("on first floor") + "\n\n"
+	msg += "The purpose is to allow you to uniquely identify this Pi.\n\n"
+	msg += color.New(color.FgYellow).Sprint("We will truncate this at 255 characters.")
+	fmt.Print(box(color.New(color.FgBlue), msg))
+
+	fmt.Print("Device tag: ")
+	reader := bufio.NewReader(os.Stdin)
+	tag, _ := reader.ReadString('\n')
+	tag = strings.TrimSpace(tag)
+
+	fmt.Println()
+	yay := box(color.New(color.FgHiGreen), color.New(color.FgGreen).Sprint("Awesome!"))
+	fmt.Print(yay)
+	time.Sleep(2 * time.Second)
+
+	return tag
+}
+
+func writeYAML(cfg *config, path string) {
+	s := fmt.Sprintf(`api_token: "%v"`, cfg.Token)
+	s += "\n"
+	s += fmt.Sprintf(`fcfs_seq_id: "%v"`, cfg.FCFS)
+	s += "\n"
+	s += fmt.Sprintf(`tag: "%v"`, cfg.Tag)
+	s += "\n"
+	// This will truncate the file if it exists.
+	f, err := os.Create(path)
 	if err != nil {
-		errors.New("cannot marshal config for writing")
+		log.Fatal("could not open config for writing")
 	}
-	fmt.Println(string(s))
+	f.WriteString(s)
+	f.Close()
 }
 
 func main() {
 
+	configPathPtr := flag.String("path", yamlPath, "Where to write the config.")
 	versionPtr := flag.Bool("version", false, "Get version and exit.")
 	readFCFSPtr := flag.Bool("fcfs-seq", false, "Read in their FCFS ID.")
 	readTokenPtr := flag.Bool("token", false, "Read in their API token.")
+	tagPtr := flag.Bool("tag", false, "A local inventory tag or identifier.")
+	allPtr := flag.Bool("all", false, "Enables all values for entry.")
 	// writeFilePtr := flag.String("path", yamlPath, "Write a YAML file with requested fields.")
 	flag.Parse()
 
@@ -176,14 +220,31 @@ func main() {
 		os.Exit(0)
 	}
 
+	if *allPtr {
+		*readFCFSPtr = true
+		*tagPtr = true
+		*readTokenPtr = true
+	}
+
 	if *readFCFSPtr {
+		fmt.Println()
 		cfg.FCFS = readFCFS()
 	}
 
+	if *tagPtr {
+		fmt.Println()
+		cfg.Tag = readTag()
+	}
+
 	if *readTokenPtr {
+		fmt.Println()
 		cfg.Token = readToken()
 	}
 
-	writeYAML(cfg)
-	fmt.Println(text.FgYellow.Sprint("All done!"))
+	// Writes to the default location, or another location
+	// if overwridden by the flag.
+	writeYAML(cfg, *configPathPtr)
+
+	fmt.Println()
+	fmt.Println(box(color.New(color.FgHiBlue), color.New(color.FgYellow).Sprint("All done!")))
 }
