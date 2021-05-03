afacbf72be801d59242e1c8692f2a04d7439bbf5
James Tranovich <james.tranovich@gsa.gov>
2021-05-03 08:59:57 -0700

    fix typo in imls path.

--- input-initial-configuration.go
+++ input-initial-configuration.go
@@ -21,7 +21,7 @@ import (
 
 const VERSION = "0.0.3"
 const lookup = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-const yamlPath = "/home/imls/auth.yaml"
+const yamlPath = "/opt/imls/auth.yaml"
 
 const states = "(AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY|AS|DC|FM|GU|MH|MP|PW|PR|VI)"
 
