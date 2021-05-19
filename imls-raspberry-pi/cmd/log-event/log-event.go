30d168c4601c161e3798eb76f32da6ccfc0f8f32
Matt Jadud <matthew.jadud@gsa.gov>
2021-05-19 13:38:19 -0400

    Read in a file as B64 for info...
    This lets us log a file to the database.

--- imls-raspberry-pi/cmd/log-event/log-event.go
+++ imls-raspberry-pi/cmd/log-event/log-event.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	b64 "encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -18,14 +19,38 @@ func isJsonOk(jsonString string) bool {
 	return err == nil
 }
 
+func readFileAsB64(path string) (string, error) {
+	if _, err := os.Stat(path); err == nil {
+		bs, err := os.ReadFile(path)
+		if err != nil {
+			return "NOREAD", err
+		}
+		sEnc := b64.StdEncoding.EncodeToString([]byte(bs))
+		return sEnc, nil
+	  } else if os.IsNotExist(err) {
+		return "FILEDOESNOTEXIST", err
+	  } else {
+		// This is a rare condition. Leave the world.
+		return "", err
+	  }
+}
+
 func main() {
 	versionPtr := flag.Bool("version", false, "Get the software version and exit.")
 	tagPtr := flag.String("tag", "event", "Event tag.")
-	infoPtr := flag.String("info", "{}", "Valid JSON for info.")
+	infoPtr := flag.String("info", "{}", "Valid JSON for info. Exclusive w/ --file.")
+	filePtr := flag.String("file", "", "Filepath to include as info. Exclusive w/ --info.")
 	configPathPtr := flag.String("config", "", "Path to config.yaml (for testing).")
 	authPathPtr := flag.String("auth", "", "Path to auth.yaml (for testing).")
 	flag.Parse()
 
+	// If they just want the version, print and exit.
+	if *versionPtr {
+		fmt.Println(version.GetVersion())
+		os.Exit(0)
+	}
+
+	// Set config paths if asked for...
 	if *configPathPtr != "" {
 		config.SetConfigPath(*configPathPtr)
 	}
@@ -34,11 +59,24 @@ func main() {
 		config.SetAuthPath(*authPathPtr)
 	}
 
-	// If they just want the version, print and exit.
-	if *versionPtr {
-		fmt.Println(version.GetVersion())
-		os.Exit(0)
+	// Make sure we're exclusive between these two flags.
+	if *filePtr != "" && *infoPtr != "{}" {
+		fmt.Println("Only one of --info or --file permitted.")
+		os.Exit(-1)
 	}
+
+	// This reuses the *infoPtr for reporting below.
+	// Saves us having more if-then statements. :shrug:
+	// Not a nice reuse...
+	if *filePtr != "" {
+		b64, err := readFileAsB64(*filePtr)
+		if err != nil {
+			fmt.Println("Could not B64 convert file. Exiting.")
+			os.Exit(-1)
+		} 
+		*infoPtr = `{"file": "` + b64 + `"}`
+	}
+
 
 	if isJsonOk(*infoPtr) {
 		logger := http.NewEventLogger(config.ReadConfig())
