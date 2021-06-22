c0582e7bd3231c925095a9a3e56238d3254a3317
Matt Jadud <matthew.jadud@gsa.gov>
2021-06-22 10:29:22 -0400

    Fixing minute tick
    That converted a second ticker to a minute ticker.
    
    Now, we have a process that ticks every minute.

--- imls-raspberry-pi/cmd/session-counting/tlp/tock_every_n.go
+++ imls-raspberry-pi/cmd/session-counting/tlp/tock_every_n.go
@@ -3,8 +3,35 @@ package tlp
 import (
 	"log"
 
+	"github.com/robfig/cron/v3"
 	"gsa.gov/18f/config"
 )
+
+func TockEveryMinute(ka *Keepalive, out chan<- bool, ch_kill <-chan Ping) {
+	log.Println("Starting tick")
+	ping, pong := ka.Subscribe("TockEveryMinute", 2)
+	// What is the best way to drive a 1-second tick?
+
+	c := cron.New()
+	_, err := c.AddFunc("*/1 * * * *", func() {
+		out <- true
+	})
+	if err != nil {
+		log.Println("cron: could not set up crontab entry")
+		log.Fatal(err.Error())
+	}
+	c.Start()
+
+	for {
+		select {
+		case <-ping:
+			pong <- "tick"
+		case <-ch_kill:
+			log.Println("Exiting TockEveryN")
+			return
+		}
+	}
+}
 
 /* PROCESS tockEveryN
  * consumes a tag (for logging purposes) as well as
