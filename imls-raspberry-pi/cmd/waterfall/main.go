655f196ea9c1b5364e0a799a0fbb611fb7971767
James Tranovich <james.tranovich@gsa.gov>
2021-07-20 13:35:23 -0400

    modify waterfall to use newer structures

--- imls-raspberry-pi/cmd/waterfall/main.go
+++ imls-raspberry-pi/cmd/waterfall/main.go
@@ -18,17 +18,18 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"gsa.gov/18f/internal/analysis"
 	"gsa.gov/18f/internal/config"
+	"gsa.gov/18f/internal/structs"
 )
 
 const PATRONMINMINS = 30
 const PATRONMAXMINS = 10 * 60
 
-func countEvents(events []analysis.WifiEvent) int {
+func countEvents(events []structs.Duration) int {
 	prev := events[0]
 	counter := 1
 
 	for _, e := range events {
-		if prev.EventId != e.EventId {
+		if prev.Id != e.Id {
 			prev = e
 			counter += 1
 		}
@@ -37,10 +38,10 @@ func countEvents(events []analysis.WifiEvent) int {
 	return counter
 }
 
-func allPatronIds(events []analysis.WifiEvent) []int {
+func allPatronIds(events []structs.Duration) []int {
 	d := make(map[int]bool)
 	for _, e := range events {
-		d[e.PatronIndex] = true
+		d[e.PatronId] = true
 	}
 	a := make([]int, 0)
 	for k := range d {
@@ -49,19 +50,19 @@ func allPatronIds(events []analysis.WifiEvent) []int {
 	return a
 }
 
-func countPatrons(events []analysis.WifiEvent) int {
+func countPatrons(events []structs.Duration) int {
 	max := 0
 
 	for _, e := range events {
-		if e.PatronIndex > max {
-			max = e.PatronIndex
+		if e.PatronId > max {
+			max = e.PatronId
 		}
 	}
 
 	return max
 }
 
-func isPatron(p analysis.WifiEvent, es []analysis.WifiEvent) int {
+func isPatron(p structs.Duration, es []structs.Duration) int {
 	var earliest time.Time
 	var latest time.Time
 
@@ -69,12 +70,13 @@ func isPatron(p analysis.WifiEvent, es []analysis.WifiEvent) int {
 	latest = time.Date(1900, 1, 1, 0, 0, 0, 0, time.UTC)
 
 	for _, e := range es {
-		if p.PatronIndex == e.PatronIndex {
-			if e.Localtime.Before(earliest) {
-				earliest = e.Localtime
+		if p.PatronId == e.PatronId {
+			start, _ := time.Parse(time.RFC3339, e.Start)
+			if start.Before(earliest) {
+				earliest = start
 			}
-			if e.Localtime.After(latest) {
-				latest = e.Localtime
+			if start.After(latest) {
+				latest = start
 			}
 		}
 	}
@@ -83,7 +85,7 @@ func isPatron(p analysis.WifiEvent, es []analysis.WifiEvent) int {
 	if diff < PATRONMINMINS {
 		return analysis.Transient
 	} else if diff > PATRONMAXMINS {
-		// log.Println("id", p.PatronIndex, "diff", diff)
+		// log.Println("id", p.PatronId, "diff", diff)
 		return analysis.Device
 	} else {
 		// log.Println("patron", p)
@@ -91,17 +93,17 @@ func isPatron(p analysis.WifiEvent, es []analysis.WifiEvent) int {
 	}
 }
 
-func getPatronFirstLast(patronId int, events []analysis.WifiEvent) (int, int) {
+func getPatronFirstLast(patronId int, events []structs.Duration) (int, int) {
 	first := 1000000000
 	last := -1000000000
 
 	for _, e := range events {
-		if e.PatronIndex == patronId {
-			if e.EventId < first {
-				first = e.EventId
+		if e.PatronId == patronId {
+			if e.Id < first {
+				first = e.Id
 			}
-			if e.EventId > last {
-				last = e.EventId
+			if e.Id > last {
+				last = e.Id
 			}
 		}
 	}
@@ -109,10 +111,11 @@ func getPatronFirstLast(patronId int, events []analysis.WifiEvent) (int, int) {
 	return first, last
 }
 
-func getEventIdTime(events []analysis.WifiEvent, eventId int) (t time.Time) {
+func getEventIdTime(events []structs.Duration, eventId int) (t time.Time) {
 	for _, e := range events {
-		if e.EventId == eventId {
-			t = e.Localtime
+		if e.Id == eventId {
+			start, _ := time.Parse(time.RFC3339, e.Start)
+			t = start
 			break
 		}
 	}
@@ -123,7 +126,7 @@ func modColor(v int, p []color.Color) color.Color {
 	return p[v%len(p)]
 }
 
-func drawLines(events []analysis.WifiEvent, dc *gg.Context, width int, height int) {
+func drawLines(events []structs.Duration, dc *gg.Context, width int, height int) {
 
 	// Draw hour lines.
 	hoursSeen := make(map[int]bool)
@@ -132,11 +135,12 @@ func drawLines(events []analysis.WifiEvent, dc *gg.Context, width int, height in
 
 	prevEvent := events[0]
 	for _, e := range events {
-		if e.EventId != prevEvent.EventId {
+		if e.Id != prevEvent.Id {
 			y += 1
 			prevEvent = e
 		}
-		currentHour := e.Localtime.Hour()
+		start, _ := time.Parse(time.RFC3339, e.Start)
+		currentHour := start.Hour()
 		if _, ok := hoursSeen[currentHour]; ok {
 			// Skip
 		} else {
@@ -158,7 +162,7 @@ func drawLines(events []analysis.WifiEvent, dc *gg.Context, width int, height in
 	}
 }
 
-func drawPoints(events []analysis.WifiEvent, dc *gg.Context, width int, height int) {
+func drawPoints(events []structs.Duration, dc *gg.Context, width int, height int) {
 
 	// Draw the points
 	y := 0
@@ -166,11 +170,11 @@ func drawPoints(events []analysis.WifiEvent, dc *gg.Context, width int, height i
 	prevEvent := events[0]
 	for _, e := range events {
 		// If the event id changes, bump our y pointer down.
-		if e.EventId != prevEvent.EventId {
+		if e.Id != prevEvent.Id {
 			y += 1
 			prevEvent = e
 		}
-		x = e.PatronIndex
+		x = e.PatronId
 		isP := isPatron(e, events)
 		if isP == analysis.Device {
 			dc.SetRGBA(0.5, 0.5, 0.5, 0.5)
@@ -182,12 +186,12 @@ func drawPoints(events []analysis.WifiEvent, dc *gg.Context, width int, height i
 			dc.Fill()
 		} else {
 			// Don't draw patron points... draw lines?
-			dc.SetColor(modColor(e.PatronIndex, palette.WebSafe))
+			dc.SetColor(modColor(e.PatronId, palette.WebSafe))
 		}
 	}
 }
 
-func drawPatronLines(events []analysis.WifiEvent, dc *gg.Context, c *analysis.Counter, width int, height int) {
+func drawPatronLines(events []structs.Duration, dc *gg.Context, c *analysis.Counter, width int, height int) {
 	x := 0
 	y := 0
 
@@ -195,12 +199,12 @@ func drawPatronLines(events []analysis.WifiEvent, dc *gg.Context, c *analysis.Co
 	// Map event IDs to y
 	eventIdToY := make(map[int]int)
 	prevEvent := events[0]
-	eventIdToY[prevEvent.EventId] = y
+	eventIdToY[prevEvent.Id] = y
 	for _, e := range events {
-		if e.EventId != prevEvent.EventId {
+		if e.Id != prevEvent.Id {
 			y += 1
 			prevEvent = e
-			eventIdToY[prevEvent.EventId] = y
+			eventIdToY[prevEvent.Id] = y
 		}
 	}
 
@@ -209,37 +213,37 @@ func drawPatronLines(events []analysis.WifiEvent, dc *gg.Context, c *analysis.Co
 	drawnPatrons := make(map[int]bool)
 	for _, e := range events {
 		// If the event id changes, bump our y pointer down.
-		if e.EventId != prevEvent.EventId {
+		if e.Id != prevEvent.Id {
 			y += 1
 			prevEvent = e
 		}
-		if _, ok := drawnPatrons[e.PatronIndex]; ok {
+		if _, ok := drawnPatrons[e.PatronId]; ok {
 			// Skip if we already checked this patron
 		} else {
-			drawnPatrons[e.PatronIndex] = true
+			drawnPatrons[e.PatronId] = true
 			isP := isPatron(e, events)
 			switch isP {
 			case analysis.Patron:
-				x = e.PatronIndex
-				first, last := getPatronFirstLast(e.PatronIndex, events)
+				x = e.PatronId
+				first, last := getPatronFirstLast(e.PatronId, events)
 				firstTime := getEventIdTime(events, first)
 				lastTime := getEventIdTime(events, last)
 				minutes := int(lastTime.Sub(firstTime).Minutes())
 				c.Add(analysis.Patron, minutes)
-				dc.SetColor(modColor(e.PatronIndex, palette.WebSafe))
+				dc.SetColor(modColor(e.PatronId, palette.WebSafe))
 				dc.DrawLine(float64(x), float64(eventIdToY[first]), float64(x), float64(eventIdToY[last]))
 				//dc.DrawPoint(float64(x), float64(y), 2)
 				//dc.Fill()
 				dc.SetLineWidth(3)
 				dc.Stroke()
 			case analysis.Device:
-				first, last := getPatronFirstLast(e.PatronIndex, events)
+				first, last := getPatronFirstLast(e.PatronId, events)
 				firstTime := getEventIdTime(events, first)
 				lastTime := getEventIdTime(events, last)
 				minutes := int(lastTime.Sub(firstTime).Minutes())
 				c.Add(analysis.Device, minutes)
 			case analysis.Transient:
-				first, last := getPatronFirstLast(e.PatronIndex, events)
+				first, last := getPatronFirstLast(e.PatronId, events)
 				firstTime := getEventIdTime(events, first)
 				lastTime := getEventIdTime(events, last)
 				minutes := int(lastTime.Sub(firstTime).Minutes())
@@ -254,7 +258,7 @@ func drawPatronLines(events []analysis.WifiEvent, dc *gg.Context, c *analysis.Co
 
 // Return the drawing context where the image is drawn.
 // This can then be written to disk.
-func drawWaterfall(events []analysis.WifiEvent) (c *analysis.Counter, dc *gg.Context) {
+func drawWaterfall(events []structs.Duration) (c *analysis.Counter, dc *gg.Context) {
 
 	// Event ids are our measure of y, patron ids are x.
 	// Colors come from mfg?
@@ -263,7 +267,7 @@ func drawWaterfall(events []analysis.WifiEvent) (c *analysis.Counter, dc *gg.Con
 	log.Println("width", width, "height", height)
 	// allIds := allPatronIds(events)
 	sort.Slice(events, func(i, j int) bool {
-		return events[i].ID < events[j].ID
+		return events[i].Id < events[j].Id
 	})
 
 	// This creates an infinite sized image of uniform color.
@@ -273,7 +277,10 @@ func drawWaterfall(events []analysis.WifiEvent) (c *analysis.Counter, dc *gg.Con
 
 	dc.SetRGBA(0.5, 0.5, 0, 0.5)
 	dc.SetLineWidth(1)
-	dc.DrawStringAnchored(fmt.Sprint(events[0].FCFSSeqId, " ", events[0].DeviceTag, " ", events[0].Localtime.Format("2006-01-02")), float64(width-5), float64(1), 1, 1)
+
+	start, _ := time.Parse(time.RFC3339, events[0].Start)
+	str := fmt.Sprint(events[0].FCFSSeqId, " ", events[0].DeviceTag, " ", start.Format("2006-01-02"))
+	dc.DrawStringAnchored(str, float64(width-5), float64(1), 1, 1)
 
 	drawLines(events, dc, width, height)
 	drawPoints(events, dc, width, height)
@@ -284,14 +291,14 @@ func drawWaterfall(events []analysis.WifiEvent) (c *analysis.Counter, dc *gg.Con
 	return c, dc
 }
 
-func readWifiEventsFromCSV(path string) []analysis.WifiEvent {
+func readWifiEventsFromCSV(path string) []structs.Duration {
 
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal("could not open CSV file.")
 	}
 
-	var events []analysis.WifiEvent
+	var events []structs.Duration
 	if err := csvutil.Unmarshal(b, &events); err != nil {
 		log.Println(err)
 		log.Fatal("could not unmarshal CSV file as wifi events.")
@@ -312,7 +319,7 @@ func buildImagePath(fcfs string, deviceTag string, pngName string) string {
 	return fullPath
 }
 
-func drawOldWaterfalls(events []analysis.WifiEvent, dataPtr *string) {
+func drawOldWaterfalls(events []structs.Duration, dataPtr *string) {
 
 	// Capture the data about the session while running in a `counter` structure.
 	c, dc := drawWaterfall(events)
@@ -352,32 +359,23 @@ func drawOldWaterfalls(events []analysis.WifiEvent, dataPtr *string) {
 	}
 }
 
-func pad(n int) string {
-	if n < 10 {
-		return fmt.Sprintf("0%v", n)
-	} else {
-		return fmt.Sprintf("%v", n)
-	}
-}
-
-func readDurationsFromSqlite(path string) []*analysis.Duration {
+func readDurationsFromSqlite(path string) []*structs.Duration {
 	db, err := sqlx.Open("sqlite3", path)
 	if err != nil {
 		log.Fatal("could not open sqlite file.")
 	}
 
-	events := []*analysis.Duration{}
+	events := []*structs.Duration{}
 	rows, err := db.Query("SELECT * FROM durations")
 	if err != nil {
 		log.Println("error in read query")
 		log.Fatal(err)
 	}
 	for rows.Next() {
-		d := analysis.Duration{}
+		d := structs.Duration{}
 		var id int
 		var minutes int
-		err = rows.Scan(&id, &d.PiSerial, &d.SessionId, &d.FCFSSeqId,
-			&d.DeviceTag, &d.PatronId, &d.MfgId, &d.Start, &d.End, &minutes)
+		err = rows.Scan(&id, &d.PiSerial, &d.SessionId, &d.FCFSSeqId, &d.DeviceTag, &d.PatronId, &d.MfgId, &d.Start, &d.End, &minutes)
 		if err != nil {
 			log.Println("could not scan")
 			log.Fatal(err)
@@ -399,7 +397,7 @@ func main() {
 		log.Fatal("Must provide valid config file.")
 	}
 
-	cfg, _ := config.ReadConfig(*cfgPath)
+	cfg, _ := config.NewConfigFromPath(*cfgPath)
 
 	if *typeFlag == "sqlite" {
 		durations := readDurationsFromSqlite(*srcPtr)
@@ -409,10 +407,10 @@ func main() {
 		}
 
 		for _, s := range sessions {
-			subset := make([]*analysis.Duration, 0)
+			subset := make([]structs.Duration, 0)
 			for _, d := range durations {
 				if d.SessionId == s {
-					subset = append(subset, d)
+					subset = append(subset, *d)
 				}
 			}
 
@@ -425,7 +423,7 @@ func main() {
 			// the actual event stream than trust the file passed.
 			cfg.Auth.FCFSId = fcfs
 			cfg.Auth.DeviceTag = dt
-			pngName := fmt.Sprintf("%v-%v-%v-%v-%v-patron-sessions", dtime.Year(), pad(int(dtime.Month())), pad(int(dtime.Day())), fcfs, dt)
+			pngName := fmt.Sprintf("%v-%.2v-%.2v-%v-%v-patron-sessions", dtime.Year(), int(dtime.Month()), int(dtime.Day()), fcfs, dt)
 			//log.Println("writing to", pngName)
 			analysis.DrawPatronSessions(cfg, subset, buildImagePath(fcfs, dt, pngName))
 		}
