cantsin/imls-pi-stack

Interfaces for DBs
Moves towards  a cleaner DB world.

imls-raspberry-pi/internal/state/geturi.go
@@ -1,4 +1,4 @@
-package config
+package state
 
 import "strings"
 
@@ -18,20 +18,16 @@ func startsWithSlash(uri string) string {
 	return "/" + uri
 }
 
-// func endsWithSlash(uri string) string {
-// 	return uri + "/"
-// }
-
-func (cfg *Config) GetLoggingUri() string {
-	var uri string = (cfg.Umbrella.Scheme + "://" +
-		removeLeadingAndTrailingSlashes(cfg.Umbrella.Host) +
-		startsWithSlash(removeLeadingSlashes(cfg.Umbrella.Logging)))
+func GetEventsUri() string {
+	var uri string = (the_config.Umbrella.Scheme + "://" +
+		removeLeadingAndTrailingSlashes(the_config.Umbrella.Host) +
+		startsWithSlash(removeLeadingSlashes(the_config.Umbrella.Paths.Events)))
 	return uri
 }
 
-func (cfg *Config) GetDataUri() string {
-	var uri string = (cfg.Umbrella.Scheme + "://" +
-		removeLeadingAndTrailingSlashes(cfg.Umbrella.Host) +
-		startsWithSlash(removeLeadingSlashes(cfg.Umbrella.Data)))
+func GetDurationsUri() string {
+	var uri string = (the_config.Umbrella.Scheme + "://" +
+		removeLeadingAndTrailingSlashes(the_config.Umbrella.Host) +
+		startsWithSlash(removeLeadingSlashes(the_config.Umbrella.Paths.Durations)))
 	return uri
 }

imls-raspberry-pi/internal/state/sessionid.go
@@ -1,45 +1,43 @@
 package state
 
 import (
-	"path/filepath"
-
-	"gsa.gov/18f/internal/config"
-	"gsa.gov/18f/internal/logwrapper"
+	"log"
+	"sync"
 )
 
-func getCurrentSessionID(cfg *config.Config) int {
-	lw := logwrapper.NewLogger(nil)
-	fullpath := filepath.Join(cfg.Local.WebDirectory, DURATIONSDB)
-	tdb := NewSqliteDB(DURATIONSDB, fullpath)
-	if tdb.CheckTableExists("durations") {
-		tdb.Open()
-		defer tdb.Close()
-		var sessionID int
-		err := tdb.Ptr.Get(&sessionID, "SELECT MAX(session_id) FROM durations")
-		if err != nil {
-			lw.Error("error in finding max session id; returning 0")
-			lw.Error(err.Error())
-			return 0
+var sid_once sync.Once
+
+func (cfg *CFG) InitializeSessionId() {
+	sid_once.Do(func() {
+		sid := 0
+		tdb := NewSqliteDB(the_config.Databases.Durations)
+		if tdb.CheckTableExists("durations") {
+			tdb.Open()
+			defer tdb.Close()
+			var sessionId int
+			err := tdb.Ptr.Get(&sessionId, "SELECT MAX(session_id) FROM durations")
+			if err != nil {
+				log.Println("error in finding max session id; returning 0")
+				log.Println(err.Error())
+			}
+			sid = sessionId
+		} else {
+			log.Println("durations table did not exist; returning session id 0")
+			sid = 0
 		}
-		return sessionID
-	} else {
-		lw.Error("durations table did not exist; returning session id 0")
-		return 0
-	}
+		the_config.SessionId = sid
+	})
 }
 
-func GetInitialSessionID(cfg *config.Config) int {
-	return getCurrentSessionID(cfg)
+func (cfg *CFG) GetCurrentSessionId() int {
+	return the_config.SessionId
 }
 
-func GetCurrentSessionID(cfg *config.Config) int {
-	return cfg.SessionId
+func (cfg *CFG) IncrementSessionId() int {
+	the_config.SessionId = the_config.SessionId + 1
+	return the_config.SessionId
 }
 
-func GetNextSessionID(cfg *config.Config) int {
-	return cfg.SessionId + 1
-}
-
-func GetPreviousSessionID(cfg *config.Config) int {
-	return cfg.SessionId - 1
+func (cfg *CFG) GetPreviousSessionID() int {
+	return the_config.SessionId - 1
 }

IMLS/estimating-wifi

imls-raspberry-pi/internal/state/geturi.go
@@ -1,4 +1,4 @@
-package config
+package state
 
 import "strings"
 
@@ -18,20 +18,16 @@ func startsWithSlash(uri string) string {
 	return "/" + uri
 }
 
-// func endsWithSlash(uri string) string {
-// 	return uri + "/"
-// }
-
-func (cfg *Config) GetLoggingURI() string {
-	var uri string = (cfg.Umbrella.Scheme + "://" +
-		removeLeadingAndTrailingSlashes(cfg.Umbrella.Host) +
-		startsWithSlash(removeLeadingSlashes(cfg.Umbrella.Logging)))
+func GetEventsURI() string {
+	var uri string = (the_config.Umbrella.Scheme + "://" +
+		removeLeadingAndTrailingSlashes(the_config.Umbrella.Host) +
+		startsWithSlash(removeLeadingSlashes(the_config.Umbrella.Paths.Events)))
 	return uri
 }
 
-func (cfg *Config) GetDataURI() string {
-	var uri string = (cfg.Umbrella.Scheme + "://" +
-		removeLeadingAndTrailingSlashes(cfg.Umbrella.Host) +
-		startsWithSlash(removeLeadingSlashes(cfg.Umbrella.Data)))
+func GetDurationsURI() string {
+	var uri string = (the_config.Umbrella.Scheme + "://" +
+		removeLeadingAndTrailingSlashes(the_config.Umbrella.Host) +
+		startsWithSlash(removeLeadingSlashes(the_config.Umbrella.Paths.Durations)))
 	return uri
 }

imls-raspberry-pi/internal/state/sessionid.go
@@ -1,45 +1,43 @@
 package state
 
 import (
-	"path/filepath"
-
-	"gsa.gov/18f/internal/config"
-	"gsa.gov/18f/internal/logwrapper"
+	"log"
+	"sync"
 )
 
-func getCurrentSessionID(cfg *config.Config) int {
-	lw := logwrapper.NewLogger(nil)
-	fullpath := filepath.Join(cfg.Local.WebDirectory, DURATIONSDB)
-	tdb := NewSqliteDB(DURATIONSDB, fullpath)
-	if tdb.CheckTableExists("durations") {
-		tdb.Open()
-		defer tdb.Close()
-		var sessionID int
-		err := tdb.Ptr.Get(&sessionID, "SELECT MAX(session_id) FROM durations")
-		if err != nil {
-			lw.Error("error in finding max session id; returning 0")
-			lw.Error(err.Error())
-			return 0
+var sid_once sync.Once
+
+func (cfg *CFG) InitializeSessionId() {
+	sid_once.Do(func() {
+		sid := 0
+		tdb := NewSqliteDB(the_config.Databases.Durations)
+		if tdb.CheckTableExists("durations") {
+			tdb.Open()
+			defer tdb.Close()
+			var sessionId int
+			err := tdb.Ptr.Get(&sessionId, "SELECT MAX(session_id) FROM durations")
+			if err != nil {
+				log.Println("error in finding max session id; returning 0")
+				log.Println(err.Error())
+			}
+			sid = sessionId
+		} else {
+			log.Println("durations table did not exist; returning session id 0")
+			sid = 0
 		}
-		return sessionID
-	} else {
-		lw.Error("durations table did not exist; returning session id 0")
-		return 0
-	}
+		the_config.SessionId = sid
+	})
 }
 
-func GetInitialSessionID(cfg *config.Config) int {
-	return getCurrentSessionID(cfg)
+func (cfg *CFG) GetCurrentSessionID() int {
+	return the_config.SessionID
 }
 
-func GetCurrentSessionID(cfg *config.Config) int {
-	return cfg.SessionID
+func (cfg *CFG) IncrementSessionID() int {
+	the_config.SessionId = the_config.SessionId + 1
+	return the_config.SessionID
 }
 
-func GetNextSessionID(cfg *config.Config) int {
-	return cfg.SessionID + 1
-}
-
-func GetPreviousSessionID(cfg *config.Config) int {
-	return cfg.SessionID - 1
+func (cfg *CFG) GetPreviousSessionID() int {
+	return the_config.SessionID - 1
 }