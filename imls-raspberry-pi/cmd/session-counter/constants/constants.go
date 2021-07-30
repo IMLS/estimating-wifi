4ac8d553bc8dfe78e01136406ee35a1bb85ed23c
James Tranovich <james.tranovich@gsa.gov>
2021-07-30 08:50:26 -0400

    remove stale constants

--- imls-raspberry-pi/cmd/session-counter/constants/constants.go
+++ imls-raspberry-pi/cmd/session-counter/constants/constants.go
@@ -1,10 +1,3 @@
 package constants
 
 const MACLENGTH = 17
-
-const ExitNoUsername = -100
-const ExitNoPassword = -101
-const ExitProcessTimeout = -200
-
-const AuthTokenKey = "AUTHTOKEN"
-const AuthEmailKey = "AUTHEMAIL"
