7b7f2947aca0065216ca71229e03d49765e91bed
James Tranovich <james.tranovich@gsa.gov>
2022-05-24 14:16:52 -0700

    add cobra and zerolog

--- imls-raspberry-pi/cmd/session-counter/session-counter.go
+++ imls-raspberry-pi/cmd/session-counter/session-counter.go
@@ -1,53 +1,35 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
-	"os"
 	"sync"
 	"time"
 
 	"github.com/robfig/cron/v3"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
 	"gsa.gov/18f/cmd/session-counter/tlp"
 	"gsa.gov/18f/internal/state"
 	"gsa.gov/18f/internal/version"
 	"gsa.gov/18f/internal/wifi-hardware-search/search"
 )
 
-func initConfigFromFlags() {
-	versionPtr := flag.Bool("version", false, "Get the software version and exit.")
-	configPathPtr := flag.String("config", "", "Path to config.sqlite. REQUIRED.")
-	flag.Parse()
-
-	// If they just want the version, print and exit.
-	if *versionPtr {
-		fmt.Println(version.GetVersion())
-		os.Exit(0)
-	}
-
-	// Make sure a config is passed.
-	if *configPathPtr == "" {
-		log.Fatal("The flag --config MUST be provided.")
-		os.Exit(1)
-	}
-
-	if _, err := os.Stat(*configPathPtr); os.IsNotExist(err) {
-		log.Println("Looked for config at ", *configPathPtr)
-		log.Fatal("Cannot find config file. Exiting.")
-	}
-
-	state.SetConfigAtPath(*configPathPtr)
-
-}
+var (
+	cfgFile  string
+	logLevel string
+)
 
 func runEvery(crontab string, c *cron.Cron, fun func()) {
-	cfg := state.GetConfig()
 	id, err := c.AddFunc(crontab, fun)
-	cfg.Log().Debug("launched crontab ", crontab, " with id ", id)
+	log.Debug().
+		Str("crontab", crontab).
+		Str("id", fmt.Sprintf("%v", id)).
+		Msg("runEvery")
 	if err != nil {
-		cfg.Log().Error("cron: could not set up crontab entry")
-		cfg.Log().Fatal(err.Error())
+		log.Fatal().
+			Str("error", err.Error()).
+			Msg("cron: could not set up crontab entry")
 	}
 }
 
@@ -60,7 +42,7 @@ func run2() {
 
 	go runEvery("*/1 * * * *", c,
 		func() {
-			cfg.Log().Debug("RUNNING SIMPLESHARK")
+			log.Debug().Msg("RUNNING SIMPLESHARK")
 			tlp.SimpleShark(
 				search.SetMonitorMode,
 				search.SearchForMatchingDevice,
@@ -69,7 +51,9 @@ func run2() {
 
 	go runEvery(cfg.GetResetCron(), c,
 		func() {
-			cfg.Log().Info("RUNNING PROCESSDATA at ", state.GetClock().Now().In(time.Local))
+			log.Info().
+				Str("time", fmt.Sprintf("%v", state.GetClock().Now().In(time.Local))).
+				Msg("RUNNING PROCESSDATA")
 			// Copy ephemeral durations over to the durations table
 			tlp.ProcessData(durationsdb, sq, iq)
 			// Draw images of the data
@@ -86,18 +70,69 @@ func run2() {
 	c.Start()
 }
 
-func main() {
-	// DO NOT USE LOGGING YET
-	initConfigFromFlags()
+func launchTLP() {
+	state.SetConfigAtPath(cfgFile)
 	cfg := state.GetConfig()
-	// NOW YOU MAY USE LOGGING.
 
-	cfg.Log().Info("Startup session id ", cfg.GetCurrentSessionID())
+	log.Info().
+		Int64("session_id", cfg.GetCurrentSessionID()).
+		Msg("session id at launch")
 
 	// Run the network
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go run2()
+
 	// Wait forever.
 	wg.Wait()
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "session-counter",
+	Short: "A tool for monitoring wifi devices while preserving privacy.",
+	Long: `session-counter watches to see what wifi devices are nearby while
+carefully leaving out information that would impose on the privacy of people
+around you.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		launchTLP()
+	},
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "session-counter version",
+	Long:  `Print the version number of session-counter`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version.GetVersion())
+	},
+}
+
+func initLogs() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	switch lvl := logLevel; lvl {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+}
+
+func main() {
+	rootCmd.PersistentFlags().StringVar(&cfgFile,
+		"config",
+		"config.sqlite3",
+		"config file (default is config.sqlite3 in current directory")
+	rootCmd.PersistentFlags().StringVar(&logLevel,
+		"logging",
+		"info",
+		"logging level (debug, info, warn, error)")
+	cobra.OnInitialize(initLogs)
+	rootCmd.AddCommand(versionCmd)
+	rootCmd.Execute()
 }
