d786aa6279bfc124bca0d59ae19c5e49cb70fe29
Anastasia Gradova <anastasia.gradova@gsa.gov>
2022-07-28 19:50:43 -0600

    Added go-resty to go.mod and created new-post.go for testing

---
+++ imls-raspberry-pi/internal/http/new-post.go
@@ -0,0 +1,61 @@
+package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty"
+)
+
+//***Define Success and Error structures***
+
+type AuthSuccess struct {
+	/* variables */
+}
+type AuthError struct {
+	/* variables */
+}
+
+//***Configuration for POST/GET***
+var timeOut int = 15
+
+//***Begin POST function***
+func newPost(uri string, data []map[string]interface{}, key string) {
+
+	//***for testing***
+	fmt.Println("Running Post")
+	myJson, _ := json.Marshal(data)
+	fmt.Println(string(myJson))
+
+	//***create client and conditions needed for the client***
+	client := resty.New()
+	client.AddRetryCondition(
+		func(r *resty.Response, err error) bool {
+			return r.StatusCode() == http.StatusUnauthorized
+		},
+	)
+	client.SetTimeout(time.Duration(timeOut) * time.Second)
+
+	//***Client post data***
+	resp, err := client.R().
+		SetBody(data).
+		SetAuthToken(key).
+		//SetResult(&AuthSuccess{}). Could be incorperated once we have defined response
+		//SetError(&AuthError{}). Could be incorperated once we have defined response
+		Post(uri)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("  Body       :\n", resp)
+	}
+
+	//***for testing response code***
+	//fmt.Println("Response Info:")
+	//fmt.Println("Status Code:", resp.StatusCode())
+	//fmt.Println("Status:", resp.Status())
+
+}
