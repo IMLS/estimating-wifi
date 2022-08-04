984662713b7800aeccb72245feec0e4f8e5e6a04
Sarah Rudder <sarah.rudder@gsa.gov>
2022-08-04 18:38:38 -0500

    swap jest for vitest, remove a bunch of deps

--- imls-frontend/src/components/USWDSContent.spec.js
+++ imls-frontend/src/components/USWDSContent.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import USWDSContent from './USWDSContent.vue'
 
-describe('USWDSContent', () => {
+test('USWDSContent', () => {
   it('should convert multiline text to separate paragraphs', () => {
     const multilineContent = 'Line 1 \n Line 2';
     const wrapper = mount(USWDSContent, { props: { multilineContent } })
--- imls-frontend/vite.config.js
+++ imls-frontend/vite.config.js
@@ -12,6 +12,23 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      'check-coverage': true,
+      reporter: ['lcovonly',  'text', 'text-summary'],
+      all: true,
+      include: [
+        "src/**/*.vue",
+      ],
+      lines: 90,
+      branches: 90,
+      functions: 90
+     
+    },
+    reporters: 'verbose'
+  }
   // base: process.env.NODE_ENV === 'production'
   // // process.env.BASEURL should be '/site/[ORG_NAME]/[REPO_NAME]' on federalist
   //   ? process.env.BASEURL + '/'
