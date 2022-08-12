42342dbda30a52d5065fd1ffd0ab25c8b5307139
Meenu Bhooshanan <meenu.bhooshanan@gsa.gov>
2022-08-12 09:37:11 -0400

    Left heartbeat table empty for now, changed hourly_ping to current timestamp

--- imls-backend/db/migrations/20220811192329_heartbeat.sql
+++ imls-backend/db/migrations/20220811192329_heartbeat.sql
@@ -4,7 +4,7 @@ CREATE TABLE imlswifi.heartbeats (
     heartbeat_id SERIAL PRIMARY KEY,
     fscs_id VARCHAR(16) NOT NULL,
     sensor_id SERIAL NOT NULL,
-    hourly_ping TIMESTAMP,
+    hourly_ping TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_heartbeat_library
         FOREIGN KEY (fscs_id)
             REFERENCES imlswifi.libraries(fscs_id),
@@ -17,10 +17,6 @@ CREATE TABLE imlswifi.heartbeats (
 CREATE INDEX fk_heartbeat_library_index ON imlswifi.heartbeats(fscs_id);
 
 CREATE INDEX fk_heartbeat_sensor_index ON imlswifi.heartbeats(sensor_id);
-
-INSERT INTO imlswifi.heartbeats(sensor_id, fscs_id)
-    SELECT presences.sensor_id, presences.fscs_id
-     FROM imlswifi.presences;
 
 -- migrate:down
 
