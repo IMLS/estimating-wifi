051ffdd22212be164fcf53002085797e15952dbd
Meenu Bhooshanan <meenu.bhooshanan@gsa.gov>
2022-08-17 15:16:50 -0400

    First draft of plpgsql function to bin devices by hour

--- imls-backend/db/schema.sql
+++ imls-backend/db/schema.sql
@@ -24,53 +24,42 @@ CREATE SCHEMA imlswifi;
 
 
 --
--- Name: bin_devices_per_hour(timestamp with time zone, text); Type: FUNCTION; Schema: api; Owner: -
+-- Name: bin_devices_per_hour(date, text); Type: FUNCTION; Schema: api; Owner: -
 --
 
-CREATE FUNCTION api.bin_devices_per_hour(_day timestamp with time zone, _fscs_id text) RETURNS TABLE(num_devices integer, hour timestamp with time zone)
+CREATE FUNCTION api.bin_devices_per_hour(_day date, _fscs_id text) RETURNS integer[]
     LANGUAGE plpgsql
     AS $$
--- Variable declarations happen in a single block.
--- Tables cannot be declared as variables.
 DECLARE
     _start TIMESTAMPTZ;
     _end TIMESTAMPTZ;
     _count INT;
-    -- _hour INT := 0;
-    -- _hour TIMESTAMPTZ:=_day;
+    _hour INT := 0;
     _period TIMESTAMPTZ;
+    num_devices_arr INT[];
 BEGIN
-    -- Temporary tables are not:
-    --   * Scoped to the function.
-    --   * Automatically cleaned up after a function execution.
-    CREATE TEMP TABLE _results (hour TIMESTAMPTZ, count INT);
-    _period := _day::TIMESTAMPTZ + '1 day'::INTERVAL;
-    WHILE _day < _period LOOP
-        -- Note that type casting is often necessary to make things work.
-        -- Here, I had to pass in a DATE object, but to add it to an interval,
-        -- it had to become a TIMESTAMP, which sets the HH:MM:SS to 00:00:00.
-        _start = _day;
-        _end =  _day + interval '1 hour';
+    -- CREATE TEMP TABLE _results (hour TIMESTAMPTZ, count INT);
+    -- _period := _day::TIMESTAMPTZ + '1 day'::INTERVAL;
+    _hour := _hour + 4;
+    -- Hardcoded EDT for now. Will add the look up table next to pass in the time zone
+    WHILE _hour < 24 LOOP
+
+        -- Casting the DATE variable to a TIMESTAMP to add it to the interval
+        _start = _day::TIMESTAMP + make_interval(hours=> _hour);
+        _end =  _day + make_interval(hours=> _hour, mins => 59, secs => 59);
+
         -- This select stores the result in the variable _count.
         SELECT count(*) INTO _count
-        FROM imlswifi.presences
+        FROM api.presences
         WHERE  fscs_id = _fscs_id
         AND (presences.start_time::TIMESTAMPTZ < presences.end_time::TIMESTAMPTZ)
         AND (presences.start_time::TIMESTAMPTZ <= _end::TIMESTAMPTZ)
         AND (presences.end_time > _start::TIMESTAMPTZ);
-        INSERT INTO _results VALUES (_day, _count);
-        -- This was a handy way to "printf" the loop counter.
-        -- RAISE NOTICE 'HOUR: %', _hour;
-        -- If you forget this, the loop will not terminate.
-        -- _hour := _hour + 1;
-        _day := _day + interval '1 hour';
+        num_devices_arr := array_append(num_devices_arr, _count);
+
+        _hour := _hour + 1;
     END LOOP;
-    -- Specify the return query.
-    RETURN QUERY SELECT * FROM _results;
-    -- Drop the temporary table.
-    DROP TABLE _results;
-    -- Return the two-column table.
-    RETURN;
+    RETURN num_devices_arr;
 END
 $$;
 
@@ -395,4 +384,4 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20220729132839'),
     ('20220729150547'),
     ('20220810132049'),
-    ('20220816180827');
+    ('20220817173135');
