362ac52238529faed38581cf6f190b33df3033c5
Meenu Bhooshanan <meenu.bhooshanan@gsa.gov>
2022-08-17 11:59:09 -0400

    Initial attempt at the stored procedure, still troubleshooting web_anon permissions

--- imls-backend/db/schema.sql
+++ imls-backend/db/schema.sql
@@ -23,6 +23,72 @@ CREATE SCHEMA api;
 CREATE SCHEMA imlswifi;
 
 
+--
+-- Name: bin_devices_per_hour(timestamp with time zone, text); Type: FUNCTION; Schema: api; Owner: -
+--
+
+CREATE FUNCTION api.bin_devices_per_hour(_day timestamp with time zone, _fscs_id text) RETURNS TABLE(num_devices integer, hour timestamp with time zone)
+    LANGUAGE plpgsql
+    AS $$
+-- Variable declarations happen in a single block.
+-- Tables cannot be declared as variables.
+DECLARE
+    _start TIMESTAMPTZ;
+    _end TIMESTAMPTZ;
+    _count INT;
+    -- _hour INT := 0;
+    -- _hour TIMESTAMPTZ:=_day;
+    _period TIMESTAMPTZ;
+BEGIN
+    -- Temporary tables are not:
+    --   * Scoped to the function.
+    --   * Automatically cleaned up after a function execution.
+    CREATE TEMP TABLE _results (hour TIMESTAMPTZ, count INT);
+    _period := _day::TIMESTAMPTZ + '1 day'::INTERVAL;
+    WHILE _day < _period LOOP
+        -- Note that type casting is often necessary to make things work.
+        -- Here, I had to pass in a DATE object, but to add it to an interval,
+        -- it had to become a TIMESTAMP, which sets the HH:MM:SS to 00:00:00.
+        _start = _day;
+        _end =  _day + interval '1 hour';
+        -- This select stores the result in the variable _count.
+        SELECT count(*) INTO _count
+        FROM imlswifi.presences
+        WHERE  fscs_id = _fscs_id
+        AND (presences.start_time::TIMESTAMPTZ < presences.end_time::TIMESTAMPTZ)
+        AND (presences.start_time::TIMESTAMPTZ <= _end::TIMESTAMPTZ)
+        AND (presences.end_time > _start::TIMESTAMPTZ);
+        INSERT INTO _results VALUES (_day, _count);
+        -- This was a handy way to "printf" the loop counter.
+        -- RAISE NOTICE 'HOUR: %', _hour;
+        -- If you forget this, the loop will not terminate.
+        -- _hour := _hour + 1;
+        _day := _day + interval '1 hour';
+    END LOOP;
+    -- Specify the return query.
+    RETURN QUERY SELECT * FROM _results;
+    -- Drop the temporary table.
+    DROP TABLE _results;
+    -- Return the two-column table.
+    RETURN;
+END
+$$;
+
+
+--
+-- Name: test(); Type: FUNCTION; Schema: api; Owner: -
+--
+
+CREATE FUNCTION api.test() RETURNS TABLE(start_time timestamp with time zone, end_time timestamp with time zone)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+RETURN QUERY
+SELECT presences.start_time, presences.end_time
+FROM api.presences;
+END; $$;
+
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -326,4 +392,5 @@ INSERT INTO public.schema_migrations (version) VALUES
     ('20220727163656'),
     ('20220729132839'),
     ('20220729150547'),
-    ('20220811194424');
+    ('20220810132049'),
+    ('20220816180827');
