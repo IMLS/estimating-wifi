cc3799aa4e9d7b76d24064272c574c82916a21c9
meenubhooshanan <meenubhooshanan@users.noreply.github.com>
2022-08-22 20:29:11 +0000

    [MegaLinter] Apply linter fixes

--- imls-frontend/vite.config.js
+++ imls-frontend/vite.config.js
@@ -6,9 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({}),
-  ],
+  plugins: [vue({})],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -28,5 +26,5 @@ export default defineConfig({
     },
     reporters: "verbose",
     setupFiles: ["test/setup.js"],
-  }  
+  },
 });
--- imls-frontend/vue.config.js
+++ imls-frontend/vue.config.js
@@ -1,8 +1,5 @@
 import process from "process";
 
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? process.env.BASEURL
-      : "/",
+  publicPath: process.env.NODE_ENV === "production" ? process.env.BASEURL : "/",
 };
