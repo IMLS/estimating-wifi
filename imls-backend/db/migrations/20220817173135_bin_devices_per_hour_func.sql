761324716334d0968a7342c542ad357b407283a7
Anastasia Gradova <anastasia.gradova@gsa.gov>
2022-09-01 20:59:02 -0600

    Updated migration for bin_devices_per_hour for parameter _day to _start.

--- imls-backend/db/migrations/20220817173135_bin_devices_per_hour_func.sql
+++ imls-backend/db/migrations/20220817173135_bin_devices_per_hour_func.sql
@@ -1,9 +1,9 @@
 -- migrate:up
 
-CREATE OR REPLACE FUNCTION api.bin_devices_per_hour(_day DATE, _fscs_id TEXT) RETURNS INT[]
+CREATE OR REPLACE FUNCTION api.bin_devices_per_hour(_start DATE, _fscs_id TEXT) RETURNS INT[]
     AS $$
 DECLARE 
-    _start TIMESTAMPTZ;
+    _init_start TIMESTAMPTZ;
     _end TIMESTAMPTZ;
     _count INT;
     _hour INT := 0;
@@ -19,7 +19,7 @@ BEGIN
     WHILE _hour < _day_end LOOP
 
         -- Casting the DATE variable to a TIMESTAMP to add it to the interval
-        _start = _day::TIMESTAMP + make_interval(hours=> _hour);
+        _init_start = _day::TIMESTAMP + make_interval(hours=> _hour);
         _end =  _day + make_interval(hours=> _hour, mins => 59, secs => 59);
 
         -- This select stores the result in the variable _count.
@@ -28,7 +28,7 @@ BEGIN
         WHERE  fscs_id = _fscs_id 
         AND (presences.start_time::TIMESTAMPTZ < presences.end_time::TIMESTAMPTZ)
         AND (presences.start_time::TIMESTAMPTZ <= _end::TIMESTAMPTZ)
-        AND (presences.end_time > _start::TIMESTAMPTZ);
+        AND (presences.end_time > _init_start::TIMESTAMPTZ);
         num_devices_arr := array_append(num_devices_arr, _count);
 
         _hour := _hour + 1;
