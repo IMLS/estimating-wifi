e53cb0b00e7cd1eb21ff96aa81026f512e60a597
Matt Jadud <matthew.jadud@gsa.gov>
2022-10-27 15:04:19 -0400

    Updating test procedure.
    One test still failing, but the README is correct.

---
+++ imls-backend/test/setup-for-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+psql ${DATABASE_URL} -v ON_ERROR_STOP=0 <<-EOSQL
+    INSERT INTO basic_auth.users VALUES ('KY0069-002', 'hello-goodbye', 'sensor') ON CONFLICT DO NOTHING;
+EOSQL
+
+psql ${DATABASE_URL} -v ON_ERROR_STOP=0 -f test-data.sql
