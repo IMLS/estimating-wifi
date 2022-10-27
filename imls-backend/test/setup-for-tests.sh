2a25b7c5dd278b7477c182a2b2db96cd9dc1209c
Matt Jadud <matthew.jadud@gsa.gov>
2022-10-27 15:18:42 -0400

    All (6) tests pass.

--- imls-backend/test/setup-for-tests.sh
+++ imls-backend/test/setup-for-tests.sh
@@ -5,3 +5,9 @@ psql ${DATABASE_URL} -v ON_ERROR_STOP=0 <<-EOSQL
 EOSQL
 
 psql ${DATABASE_URL} -v ON_ERROR_STOP=0 -f test-data.sql
+
+psql ${DATABASE_URL} -v ON_ERROR_STOP=0 <<-EOSQL
+    INSERT INTO imlswifi.imls_lookup (fscs_id, timezone) (
+        SELECT DISTINCT fscs_id,'00:00:00-04'::TIMETZ FROM imlswifi.libraries
+    );
+EOSQL
