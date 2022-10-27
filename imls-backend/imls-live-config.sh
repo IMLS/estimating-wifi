7c19666b867b1e39f9b485619702783e65154f8b
Matt Jadud <matthew.jadud@gsa.gov>
2022-10-27 11:49:55 -0400

    Shell inheritance fixed...
    This runs the static init once at boot, and walks through the /runtime
    directory to run ephemeral elements.
    
    We can conditionally run dev/prod things this way. Not yet implemented.
    Might be a bad idea.
    
    Now loads the secret into the DB after the DB starts.

--- imls-backend/imls-live-config.sh
+++ imls-backend/imls-live-config.sh
@@ -5,9 +5,10 @@ set -e
 # The image this is embedded in runs after the DB comes to life.
 # Then, we can inject things into the DB at runtime.
 
-echo --- POSTGRES URI ---
-echo ${POSTGRES_DB_URI}
-echo --------------------
+
+echo --------------------------
+echo LIVE CONFIG SQL SCRIPTS...
+echo --------------------------
 
 for FILE in `ls /runtime`; do   
     
@@ -20,11 +21,14 @@ for FILE in `ls /runtime`; do
         echo --------------------------
         psql ${POSTGRES_DB_URI} -a -f /runtime/${FILE}
     fi
-    if [[ $FILE == *.sh ]]
-    then
-        echo --------------------------
-        echo SHELL :: $FILE
-        echo --------------------------
-        source /runtime/${FILE}
-    fi
 done
+
+echo --------------------------
+echo LIVE CONFIG ONE-OFFS...
+echo --------------------------
+
+psql ${POSTGRES_DB_URI} -v ON_ERROR_STOP=1 <<-EOSQL
+    ALTER DATABASE imls SET "app.jwt_secret" TO "${POSTGRES_JWT_SECRET}";
+    INSERT INTO basic_auth.users VALUES ('KY0069-002', 'hello-goodbye', 'sensor');
+    NOTIFY pgrst, 'reload schema'
+EOSQL
