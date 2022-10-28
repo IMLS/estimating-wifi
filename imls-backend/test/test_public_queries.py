7155529a900882b9cf1ebb6633e3ed408a6c6a79
Matt Jadud <matthew.jadud@gsa.gov>
2022-10-28 10:47:29 -0400

    Test non-library lookup
    A library that does not exist returns None.

--- imls-backend/test/test_public_queries.py
+++ imls-backend/test/test_public_queries.py
@@ -16,6 +16,13 @@ class IMLSTests(TestCase):
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r.json()['time'], "00:00:00-04")
 
+    def test_timezone_lookup_fail(self):
+        url = endpoint(["rpc", "get_library_timezone"])
+        query = {"fscs_id": "KY0069-003"}
+        r = requests.post(url, json=query)
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.json()['time'], None)
+
     # NOTE: There's a ?limit parameter on these, because otherwise a lot of 
     # values come back by default, and that makes for  slow tests.
     def test_existence_of_presences(self):
