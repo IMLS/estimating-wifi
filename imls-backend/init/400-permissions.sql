1b2acd897f2d6b65d557e3dbf89e670ad8b81493
Matt Jadud <matthew.jadud@gsa.gov>
2022-11-01 12:20:31 -0400

    Adding a permission to the table for web_anon

--- imls-backend/init/400-permissions.sql
+++ imls-backend/init/400-permissions.sql
@@ -1,6 +1,7 @@
 
 GRANT USAGE ON SCHEMA api TO web_anon;
 GRANT USAGE ON SCHEMA data TO web_anon;
+GRANT SELECT ON TABLE data.imls_data TO web_anon;
 
 GRANT SELECT ON TABLE api.imls_lookup TO web_anon;
 GRANT SELECT ON TABLE api.presences TO web_anon;
