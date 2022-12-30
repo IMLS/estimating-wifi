63c8bea31582822c22a2cc14545ac5819527325e
Anastasia Gradova <anastasia.gradova@gsa.gov>
2022-12-30 08:42:06 -0500

    removed imls_lookup permission

--- imls-backend/init/400-permissions.sql
+++ imls-backend/init/400-permissions.sql
@@ -2,8 +2,6 @@
 GRANT USAGE ON SCHEMA api TO web_anon;
 GRANT USAGE ON SCHEMA data TO web_anon;
 GRANT SELECT ON TABLE data.imls_data TO web_anon;
-
-GRANT SELECT ON TABLE api.imls_lookup TO web_anon;
 GRANT SELECT ON TABLE api.presences TO web_anon;
 
 -- Public 
