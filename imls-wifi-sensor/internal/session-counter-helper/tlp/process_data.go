2375a00e9e25f4d1f405ddc04c73ed0abe5a659f
Ben Klaas <ben.klaas@gsa.gov>
2023-01-07 09:24:39 -0600

    Do our MAC logging in ephemeral.go

--- imls-wifi-sensor/internal/session-counter-helper/tlp/process_data.go
+++ imls-wifi-sensor/internal/session-counter-helper/tlp/process_data.go
@@ -18,13 +18,6 @@ func ProcessData(dDB *state.DurationsDB, sq *state.Queue[int64]) bool {
 	}
 
 	macs := state.GetMACs()
-	// FIGURE OUT FORMAT OF GetMACs return value
-	for mac, element := range macs {
-		log.Debug().
-			Int64("start", element.Start).
-			Int64("end", element.End).
-			Msg(mac)
-	}
 	dDB.InsertMany(session, macs)
 	return true
 }
