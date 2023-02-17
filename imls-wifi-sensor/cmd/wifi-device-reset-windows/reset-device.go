aa1b40b9122bc0f432bdfd72851030a6b90127a3
Ben Klaas <ben.klaas@gsa.gov>
2023-02-17 16:06:50 -0600

    simplify reset-device.go

--- imls-wifi-sensor/cmd/wifi-device-reset-windows/reset-device.go
+++ imls-wifi-sensor/cmd/wifi-device-reset-windows/reset-device.go
@@ -4,21 +4,16 @@ package main
 import (
 	"os"
 
-	"golang.org/x/sys/windows"
 	"gsa.gov/18f/internal/wifi-hardware-search/netadapter"
 	"gsa.gov/18f/internal/wifi-hardware-search/search"
 )
 
 func main() {
-	title := windows.StringToUTF16Ptr("Test adapter reset")
 	device := search.SearchForMatchingDevice()
 	if device.Exists {
 		netadapter.RestartNetAdapter(device.Logicalname)
-		windows.MessageBox(0, device.Logicalname, title, windows.MB_OK)
 		os.Exit(0)
 	} else {
-		errorMessage := windows.StringToUTF16Ptr("No device found to reset")
-		windows.MessageBox(0, errorMessage, title, windows.MB_ICONWARNING)
 		os.Exit(1)
 	}
 }
