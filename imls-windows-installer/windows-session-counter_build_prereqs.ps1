45f8f03c63aab48820f74b4930b03a9f72bff7c6
Ben Klaas <ben.klaas@gsa.gov>
2023-02-17 10:55:15 -0600

    run new powershell script as prereq

--- imls-windows-installer/windows-session-counter_build_prereqs.ps1
+++ imls-windows-installer/windows-session-counter_build_prereqs.ps1
@@ -4,3 +4,4 @@
 & "$PSScriptRoot\install_innosetup.ps1"
 & "$PSScriptRoot\build_windows-session-counter.ps1"
 & "$PSScriptRoot\build_wifi-hw-search.ps1"
+& "$PSScriptRoot\build_wifi-device-reset.ps1"
