87d4866dffe8cd086d2eae17fe2fb1fdd1dab076
Ben Klaas <ben.klaas@gsa.gov>
2023-03-22 15:40:50 -0500

    more flailing

--- imls-wifi-sensor/make_all.sh
+++ imls-wifi-sensor/make_all.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #VERSION := $(shell git describe --tags --abbrev=0)
-LDFLAGS="-X gsa.gov/18f/version.Semver=v3.5"
-ENVVARS='GOOS=linux GOARCH=arm GOARM=7'
-${ENVVARS} go install -ldflags $(LDFLAGS) gsa.gov/18f/cmd/linux-session-counter
-${ENVVARS} go install -ldflags $(LDFLAGS) gsa.gov/18f/cmd/wifi-hardware-search-cli
+#LDFLAGS="-X gsa.gov/18f/version.Semver=v3.5"
+#ENVVARS='GOOS=linux GOARCH=arm GOARM=7'
+
+GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X gsa.gov/18f/version.Semver=v3.5 gsa.gov/18f/cmd/linux-session-counter
+GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X gsa.gov/18f/version.Semver=v3.5 gsa.gov/18f/cmd/wifi-hardware-search-cli
