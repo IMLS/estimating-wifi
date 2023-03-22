983c30e5517367bf225782d727073fdd2b37e13e
Ben Klaas <ben.klaas@gsa.gov>
2023-03-22 16:23:45 -0500

    try to migrate back to a Makefile

--- imls-wifi-sensor/make_all.sh
+++ imls-wifi-sensor/make_all.sh
@@ -10,6 +10,5 @@
 #GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/wifi-hardware-search-cli
 
 go mod download
-echo "FOOBAR"
-#GOOS=linux GOARCH=arm GOARM=7 go install imls-wifi-sensor/cmd/linux-session-counter
-#GOOS=linux GOARCH=arm GOARM=7 go install imls-wifi-sensor/cmd/wifi-hardware-search-cli
+GOOS=linux GOARCH=arm GOARM=7 go install imls-wifi-sensor/cmd/linux-session-counter
+GOOS=linux GOARCH=arm GOARM=7 go install imls-wifi-sensor/cmd/wifi-hardware-search-cli
