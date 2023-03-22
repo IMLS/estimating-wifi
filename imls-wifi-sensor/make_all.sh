1980aca2986ee6611724ce97de9f8d3f1e6bc3cc
Ben Klaas <ben.klaas@gsa.gov>
2023-03-22 15:46:45 -0500

    another try

--- imls-wifi-sensor/make_all.sh
+++ imls-wifi-sensor/make_all.sh
@@ -6,5 +6,8 @@
 
 #GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X "gsa.gov/18f/version.Semver=v3.5" gsa.gov/18f/cmd/linux-session-counter
 #GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X "gsa.gov/18f/version.Semver=v3.5" gsa.gov/18f/cmd/wifi-hardware-search-cli
-GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/linux-session-counter
-GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/wifi-hardware-search-cli
+#GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/linux-session-counter
+#GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/wifi-hardware-search-cli
+
+GOOS=linux GOARCH=arm GOARM=7 go install cmd/linux-session-counter
+GOOS=linux GOARCH=arm GOARM=7 go install cmd/wifi-hardware-search-cli
