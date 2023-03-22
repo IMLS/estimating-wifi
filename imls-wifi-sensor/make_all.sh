73492adc348454c0872867369ecf2df603b95800
Ben Klaas <ben.klaas@gsa.gov>
2023-03-22 15:43:38 -0500

    remove ldflags

--- imls-wifi-sensor/make_all.sh
+++ imls-wifi-sensor/make_all.sh
@@ -4,5 +4,7 @@
 #LDFLAGS="-X gsa.gov/18f/version.Semver=v3.5"
 #ENVVARS='GOOS=linux GOARCH=arm GOARM=7'
 
-GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X "gsa.gov/18f/version.Semver=v3.5" gsa.gov/18f/cmd/linux-session-counter
-GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X "gsa.gov/18f/version.Semver=v3.5" gsa.gov/18f/cmd/wifi-hardware-search-cli
+#GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X "gsa.gov/18f/version.Semver=v3.5" gsa.gov/18f/cmd/linux-session-counter
+#GOOS=linux GOARCH=arm GOARM=7 go install -ldflags -X "gsa.gov/18f/version.Semver=v3.5" gsa.gov/18f/cmd/wifi-hardware-search-cli
+GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/linux-session-counter
+GOOS=linux GOARCH=arm GOARM=7 go install gsa.gov/18f/cmd/wifi-hardware-search-cli
